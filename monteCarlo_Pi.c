e0e51d5f88d6841d7ea99468daf830665c94062b
grace nansamba <gracenansamba@gmail.com>
2024-01-22 15:10:42 -0600

    harmonize right before the collective

--- monteCarlo_Pi.c
+++ monteCarlo_Pi.c
@@ -10,20 +10,20 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    long long total_iterations = 100;
-    long long total_points = 100000;
-    long long points_inside_circle = 0;
+    long total_iterations = 1000;
+    long total_points = 100000;
+    long points_inside_circle = 0;
     double start_time, end_time, start_compute_time, end_compute_time;;
     // Seed the random number generator differently for each process
     srand(time(NULL) + rank);
     start_time = MPI_Wtime();  
 
-    for (long long iter = 0; iter < total_iterations; iter++) {
+    for (long iter = 0; iter < total_iterations; iter++) {
 	 
 	start_compute_time = MPI_Wtime();
 	points_inside_circle = 0;;
 
-        for (long long i = 0; i < total_points; i++) {
+        for (long i = 0; i < total_points; i++) {
             double x = ((double)rand() / RAND_MAX) * 2 - 1;
             double y = ((double)rand() / RAND_MAX) * 2 - 1;
             if (x*x + y*y <= 1) {
@@ -32,9 +32,9 @@ int main(int argc, char** argv) {
         }
 
         // Sum up the counts from all processes at the end of each iteration
-        long long total_inside_circle;
+        long total_inside_circle;
 	
-	//MPI_Barrier(MPI_COMM_WORLD);
+        //MPI_Barrier(MPI_COMM_WORLD);
         MPIX_Harmonize(MPI_COMM_WORLD, &flag);
 
         MPI_Reduce(&points_inside_circle, &total_inside_circle, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
