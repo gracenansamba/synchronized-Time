74b0b5aef988d2a4f38040ea5ae672e63a432e4f
grace nansamba <gracenansamba@gmail.com>
2024-01-25 15:57:25 -0600

    large_reduction

---
+++ sdl_allreduce.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <time.h>
+#include <mpix_harmonize.h>
+
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+    int rank, size, flag;
+    int VECTOR_SIZE;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    // Create a random vector on each process
+    //int local_vector[VECTOR_SIZE];
+    if(argc !=2) {
+       	if(rank==0) printf("\nUsage: mpirun -np NP vectorSize \n");
+	MPI_Finalize();
+	exit(1);
+    }
+    else {
+       	sscanf(argv[1],"%d",&VECTOR_SIZE);	
+    }
+
+    int local_vector[VECTOR_SIZE];
+
+    srand(rank);
+    for (int i = 0; i < VECTOR_SIZE; ++i) {
+        local_vector[i] = rand() % 10;
+    }
+    double start_sync_time = MPI_Wtime();
+    MPIX_Harmonize(MPI_COMM_WORLD, &flag);
+    //MPI_Barrier(MPI_COMM_WORLD);
+    double end_sync_time = MPI_Wtime();
+    double start_time = MPI_Wtime();
+
+    // Sum the vectors using MPI_Allreduce
+    int global_vector[VECTOR_SIZE];
+
+    MPI_Allreduce(local_vector, global_vector, VECTOR_SIZE, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    //MPI_Barrier(MPI_COMM_WORLD);
+    double end_time = MPI_Wtime();
+    // Print the result and timing on each process
+   /* printf("Process %d - Local Vector: ", rank);
+    for (int i = 0; i < VECTOR_SIZE; ++i) {
+        printf("%d ", local_vector[i]);
+    }
+    printf("\n");
+    printf("Process %d - Global Vector (Sum): ", rank);
+   for (int i = 0; i < VECTOR_SIZE; ++i) {
+        printf("%d ", global_vector[i]);
+    }
+    printf("\n");*/
+    if (rank == 0) {    
+	double sync_time =  end_sync_time -start_sync_time;;
+        double program_time = end_time - start_time;
+       
+        printf("Synchronization Time: %f seconds\n", sync_time);
+        printf("AllReduce Time: %f seconds\n", program_time);
+    }
+    MPI_Finalize();
+    return 0;
+}
+
+
